0e1ada50b2c4c6774411f4ac12511808953b9847
Jackljk <94715891+jackljk@users.noreply.github.com>
2024-02-29 01:25:20 -0800

    Updating DKL one file and validations

---
+++ DKL Gaussian Process/DKL-Run-Model-Script/args.py
@@ -0,0 +1,20 @@
+import argparse
+import yaml
+
+def args():
+    parser = argparse.ArgumentParser(description='Description of your program')
+    parser.add_argument('-c', '--config', help='Path to config file')
+    parser.add_argument('-v', '--verbose', action='store_true', help='Enable verbose mode')
+
+    args = parser.parse_args()
+
+    # Access the values of the arguments
+    config_file = args.config
+    verbose_mode = args.verbose
+
+    # Read the YAML config file
+    with open(config_file, 'r') as f:
+        config = yaml.safe_load(f)
+
+    return config, verbose_mode
+
---
+++ DKL Gaussian Process/DKL-Run-Model-Script/helper_funcs.py
@@ -0,0 +1,201 @@
+import numpy as np
+import pandas as pd
+import xarray as xr
+from eofs.xarray import Eof
+from xskillscore import rmse
+import tensorflow as tf
+from tensorflow import keras
+import math
+from gpflow.kernels import Matern52, Matern32, Matern12, SquaredExponential, Exponential, RationalQuadratic, Cosine, Periodic, Polynomial, ArcCosine
+
+min_co2, max_co2 = 0.0 , 9500
+
+
+def normalize_co2(data):
+    return data / max_co2
+
+def un_normalize_co2(data):
+    return data * max_co2
+
+min_ch4, max_ch4 = 0.0, 0.8
+
+def normalize_ch4(data):
+    return data / max_ch4
+
+def un_normalize_ch4(data):
+    return data * max_ch4
+
+
+def create_predictor_data(data_sets, data_path, n_eofs=5):
+    """
+    Args:
+        data_sets list(str): names of datasets
+        n_eofs (int): number of eofs to create for aerosol variables
+    """
+        
+    # Create training and testing arrays
+    if isinstance(data_sets, str):
+        data_sets = [data_sets]
+    X = xr.concat([xr.open_dataset(data_path + f"inputs_{file}.nc") for file in data_sets], dim='time')
+    X = X.assign_coords(time=np.arange(len(X.time)))
+
+    # Compute EOFs for BC
+    bc_solver = Eof(X['BC'])
+    bc_eofs = bc_solver.eofsAsCorrelation(neofs=n_eofs)
+    bc_pcs = bc_solver.pcs(npcs=n_eofs, pcscaling=1)
+
+    # Compute EOFs for SO2
+    so2_solver = Eof(X['SO2'])
+    so2_eofs = so2_solver.eofsAsCorrelation(neofs=n_eofs)
+    so2_pcs = so2_solver.pcs(npcs=n_eofs, pcscaling=1)
+
+    # Convert to pandas
+    bc_df = bc_pcs.to_dataframe().unstack('mode')
+    bc_df.columns = [f"BC_{i}" for i in range(n_eofs)]
+
+    so2_df = so2_pcs.to_dataframe().unstack('mode')
+    so2_df.columns = [f"SO2_{i}" for i in range(n_eofs)]
+
+    # Bring the emissions data back together again and normalise
+    inputs = pd.DataFrame({
+        "CO2": normalize_co2(X["CO2"].data),
+        "CH4": normalize_ch4(X["CH4"].data)
+    }, index=X["CO2"].coords['time'].data)
+
+    # Combine with aerosol EOFs
+    inputs = pd.concat([inputs, bc_df, so2_df], axis=1)
+    return inputs, (so2_solver, bc_solver)
+
+
+def get_test_data(file, eof_solvers, n_eofs=5):
+    """
+    Args:
+        file str: name of datasets
+        n_eofs (int): number of eofs to create for aerosol variables
+        eof_solvers (Eof_so2, Eof_bc): Fitted Eof objects to use for projection
+    """
+        
+    # Create training and testing arrays
+    X = xr.open_dataset(data_path + f"inputs_{file}.nc")
+        
+    so2_pcs = eof_solvers[0].projectField(X["SO2"], neofs=5, eofscaling=1)
+    so2_df = so2_pcs.to_dataframe().unstack('mode')
+    so2_df.columns = [f"SO2_{i}" for i in range(n_eofs)]
+
+    bc_pcs = eof_solvers[1].projectField(X["BC"], neofs=5, eofscaling=1)
+    bc_df = bc_pcs.to_dataframe().unstack('mode')
+    bc_df.columns = [f"BC_{i}" for i in range(n_eofs)]
+
+    # Bring the emissions data back together again and normalise
+    inputs = pd.DataFrame({
+        "CO2": normalize_co2(X["CO2"].data),
+        "CH4": normalize_ch4(X["CH4"].data)
+    }, index=X["CO2"].coords['time'].data)
+
+    # Combine with aerosol EOFs
+    inputs = pd.concat([inputs, bc_df, so2_df], axis=1)
+    return inputs
+
+
+def create_predictdand_data(data_sets):
+    if isinstance(data_sets, str):
+        data_sets = [data_sets]
+    Y = xr.concat([xr.open_dataset(data_path + f"outputs_{file}.nc") for file in data_sets], dim='time').mean("member")
+    # Convert the precip values to mm/day
+    Y["pr"] *= 86400
+    Y["pr90"] *= 86400
+    return Y
+
+
+def get_rmse(truth, pred):
+    weights = np.cos(np.deg2rad(truth.lat))
+    return np.sqrt(((truth - pred)**2).weighted(weights).mean(['lat', 'lon'])).data
+
+def global_mean(ds):
+    weights = np.cos(np.deg2rad(ds.lat))
+    return ds.weighted(weights).mean(['lat', 'lon'])
+
+def global_sum(ds):
+    weights = np.cos(np.deg2rad(ds.lat))
+    return ds.weighted(weights).sum(['lat', 'lon'])
+
+def get_weights(truth):
+    weights = np.cos(np.deg2rad(truth.lat)).expand_dims(lon=144).assign_coords(lon=truth.lon)
+    return weights
+    
+def TNRMSE(truth, pred, Yearslice=slice(2080, None)):
+    # R2E Score
+    R2E = rmse(
+        global_mean(truth.sel(time=Yearslice)), 
+        global_mean(pred.sel(time=Yearslice))
+    ).data/np.abs(global_mean(truth.sel(time=Yearslice).mean('time')).data)
+
+    # Get weights
+    weights = get_weights(truth)
+
+    # NRMSE score
+    NRMSE = rmse(
+        truth.sel(time=Yearslice).mean('time'), 
+        pred.sel(time=Yearslice).mean('time'), weights=weights
+    ).data/ np.abs(global_mean(truth.sel(time=Yearslice).mean('time')).data)
+
+    total = (NRMSE + 5*R2E).T.round(3)
+    return total
+
+def make_feature_extractor(dim_max, activation, input_dim, output_dim, dropout_prob, bnorm=True, dropout=True):
+    model = tf.keras.Sequential()
+    model.add(keras.layers.InputLayer(input_shape=(input_dim,)))
+    
+    dimensions = [dim_max // (2 ** i) for i in range(int(math.log(dim_max, 2)) + 1) if dim_max // (2 ** i) > input_dim]
+
+    for dim in dimensions[::-1]:
+        model.add(keras.layers.Dense(dim, activation=activation))
+        if bnorm:
+            model.add(keras.layers.BatchNormalization())
+        if dropout:
+            model.add(keras.layers.Dropout(dropout_prob))
+
+    for dim in dimensions:
+        if dim <= output_dim:
+            break
+        model.add(keras.layers.Dense(dim, activation=activation))
+        if bnorm:
+            model.add(keras.layers.BatchNormalization())
+        if dropout:
+            model.add(keras.layers.Dropout(dropout_prob))
+
+    model.add(keras.layers.Dense(output_dim, activation='linear'))
+    model.add(keras.layers.Lambda(lambda x: tf.cast(x, tf.float64)))
+        
+    return model
+
+
+def basekernel(kernel_types, active_dim_multiplier, variance=1.0, lengthscales=1.0):
+    kernels = {
+        'Matern12': Matern12, 'Matern32': Matern32, 'Matern52': Matern52, 'SquaredExponential': SquaredExponential, 'RationalQuadratic': RationalQuadratic, 'Cosine':Cosine, 'Periodic': Periodic,
+        'Polynomial': Polynomial, 'ArcCosine': ArcCosine, 'Exponential': Exponential
+    }
+    dims = [x for x in range(12*active_dim_multiplier)]
+    def CO2(kernel):
+        active_dims = dims[0:active_dim_multiplier*1]
+        k = kernel(active_dims = active_dims, variance=variance, lengthscales=(1*active_dim_multiplier) * [1.])
+        return k
+
+    def CH4(kernel):
+        active_dims = dims[1*active_dim_multiplier:active_dim_multiplier*2]
+        k = kernel(active_dims = active_dims, variance=variance, lengthscales=(1*active_dim_multiplier) * [1.])
+        return k
+        
+    def BC(kernel):
+        active_dims = dims[2*active_dim_multiplier:active_dim_multiplier*2 + 5*active_dim_multiplier]
+        k = kernel(active_dims = active_dims, variance=variance, lengthscales=(5*active_dim_multiplier) * [1.])
+        return k
+
+    def SO2(kernel):
+        active_dims = dims[active_dim_multiplier*2 + 5*active_dim_multiplier:]
+        k = kernel(active_dims = active_dims, variance=variance, lengthscales=(5*active_dim_multiplier) * [1.])
+        return k
+    
+    kernel = CO2(kernels[kernel_types[0]]) + CH4(kernels[kernel_types[1]]) + BC(kernels[kernel_types[2]]) + SO2(kernels[kernel_types[3]])
+
+    return kernel
